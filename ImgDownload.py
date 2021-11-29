198ef82f4be54850caf3e8e22f0131be682ba29f
Tione6296 <59222068+Tione6296@users.noreply.github.com>
2021-11-30 07:32:40 +0900

    Update ImgDownload.py

--- ImgDownload.py
+++ ImgDownload.py
@@ -6,8 +6,8 @@ import urllib.request
 
 # range 4376 , url_list[100:4376]
 path = os.getcwd()
-
-url_list = ['https://static.image.mihoyo.com/hsod2_webview/images/broadcast_top/equip_icon/png/{:03}.png'.format(i) for i in range(4376)]
+url_list = ['https://static-image.benghuai.com/hsod2_webview/images/broadcast_top/equip_icon/png/{:03}.png'.format(i) for i in range(4376)]
+#url_list = ['https://static.image.mihoyo.com/hsod2_webview/images/broadcast_top/equip_icon/png/{:03}.png'.format(i) for i in range(4376)]
 sleep_time_sec = .2
 
 def download_file(url, path):
